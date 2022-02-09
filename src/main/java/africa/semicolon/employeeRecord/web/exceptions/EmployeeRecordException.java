a2e19d4ef4c5e75283022351d95210c2840016cd
Tosin Akinnusi <akinnusitosinbenedict@gmail.com>
2022-02-09 12:33:56 +0100

    Employee record exception

---
+++ src/main/java/africa/semicolon/employeeRecord/web/exceptions/EmployeeRecordException.java
@@ -0,0 +1,7 @@
+package africa.semicolon.employeeRecord.web.exceptions;
+
+public class EmployeeRecordException extends Throwable {
+    public EmployeeRecordException(String message) {
+        super(message);
+    }
+}
