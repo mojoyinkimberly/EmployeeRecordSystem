b0e816d6fd7d9c93c60b9a19b750eaee7a7549fb
Mojoyinola Oketope <m.oketope@semicolon.africa>
2022-02-10 09:18:20 +0100

    added new files

---
+++ src/main/resources/db/insert.sql
@@ -0,0 +1,7 @@
+
+truncate table employee;
+
+insert into employee(id, address, email, first_name, last_name, phone_number)
+values (30, 'Funsho-link', 'gidi@gmail.com', 'Gideon', 'Udoh', '08012345678'),
+       (31, 'Alara', 'oketope@gmail.com', 'Mojo', 'Oke', '07012345678'),
+       (32, 'Sabo-Yaba', 'ahmad@gmail.com', 'Ahmad', 'Ajala', '08123456789');
